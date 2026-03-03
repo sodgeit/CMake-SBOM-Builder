52cd66a7367c004f731a1e6eed56748ef3f7b042
Avus-c <48911667+Avus-c@users.noreply.github.com>
2026-03-03 23:45:05 +0100

    wip(test): restructuring of the integration tests
    using fixtures for a better testing approach and workflow
    The filepath of the generated sbom is giving me a bunch of
    headeachs. It's not possible to get the filepath where i need it,
    and wildcard don't work for some reason. Probably cmakes "fault".
    The only documented "Wildcards are not supported" in for cmake -E copy,
    which is not at all what i am doing in add_test ...

--- test/integration/testcases/file.cmake
+++ test/integration/testcases/file.cmake
@@ -12,14 +12,7 @@ install(FILES ${CMAKE_CURRENT_LIST_FILE} DESTINATION .)
 
 # Does not exist before installing.
 sbom_add_file(CMakeLists.txt FILETYPE OTHER)
-
-# Twice the same file, should not conflict.
-sbom_add_file(CMakeLists.txt FILETYPE OTHER)
 set(_id1 "${SBOM_LAST_SPDXID}")
-
-# Once more, with specified SPDXID.
-sbom_add_file(CMakeLists.txt FILETYPE OTHER SPDXID SPDXRef-again)
-set(_id2 "${SBOM_LAST_SPDXID}")
 
 file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/doc.txt" "docstring...")
 install(FILES "${CMAKE_CURRENT_BINARY_DIR}/doc.txt" DESTINATION .)
@@ -28,8 +21,8 @@ sbom_add_file("doc.txt"
 	FILETYPE DOCUMENTATION
 	RELATIONSHIP
 		"\@SBOM_LAST_SPDXID\@ DOCUMENTATION_OF ${_id1}"
-		"\@SBOM_LAST_SPDXID\@ DOCUMENTATION_OF ${_id2}"
 )
+
 
 sbom_finalize()
 
